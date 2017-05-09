2b7245aa5de78b955517f648480ab91da53f61d7
RobertH-W-Chiang <johnmay0629@gmail.com>
2017-05-09 22:25:29 +0800

    add exercise 1.7 to 1.9

---
+++ chapter01/1.7/main.go
@@ -0,0 +1,28 @@
+/*
+使用 io.Copy(dst, src) 代替 ioutil.ReadAll 來複製內容到 os.Stdout 而無須緩衝區保存整個串流。要確定有檢察 io.Copy 的錯誤
+*/
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		_, err = io.Copy(os.Stdout, resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+	}
+}
